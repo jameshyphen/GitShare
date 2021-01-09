537c81216d77989d608f784ad0f3e7b9ddb62cda
Dzhem Aptula <dzhem.aptula@gmail.com>
2021-01-09 06:24:04 +0100

    Implemented the svg creation and works with posts currently

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import base64
+from json.decoder import JSONDecodeError
+from typing import List
 from flask import Flask, Response, jsonify, render_template, request
+import requests
 
 app = Flask(__name__)
 
-def make_SVG():
-    # ...
+def make_SVG(theme, posts):
+    data_dict = {
+        "posts": posts,
+        "theme": theme
+    }
 
-    return render_template("index.html")
+    return render_template("index.html", **data_dict)
 
 
 @app.route("/", defaults={"path": ""})
@@ -15,7 +22,22 @@ def catch_all(path):
     image = request.args.get("image") or "default"
     user = request.args.get("user") or "GitShareUser"
 
-    svg = make_SVG()
+    url = f"https://raw.githubusercontent.com/{user}/{user}/master/GitShare.json"
+
+    response = requests.get(url)
+    try:
+        posts = response.json()["Posts"]
+
+    except JSONDecodeError as e:
+        posts = {
+                    "title": "Error",
+                    "description": "GitShare json on your personal profile is either invalid or missing.",
+                    "image": "https://i.imgur.com/TxAh3ln.png"
+        }
+
+    print(posts)
+
+    svg = make_SVG(theme, posts)
 
     resp = Response(svg, mimetype="image/svg+xml")
     resp.headers["Cache-control"] = "s-maxage=1"
